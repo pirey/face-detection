2f7ec750f98c0b8e566059c1cbc864657faf9d58
Yeri <arifyeripratama@gmail.com>
2018-08-13 00:16:00 +0700

    2nd init

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default class App extends React.Component {
-  render() {
+  render () {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Face Detection App</Text>
       </View>
-    );
+    )
   }
 }
 
@@ -18,6 +16,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
