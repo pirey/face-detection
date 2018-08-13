dcdd83475ba8a1be50a443c50b6fa16447bd8a97
Yeri <arifyeripratama@gmail.com>
2018-08-14 06:13:37 +0700

    app id & key from cameria

--- src/stores/recognition/actions.js
+++ src/stores/recognition/actions.js
@@ -1,3 +1,5 @@
+// credentials from https://cameria.herokuapp.com/
+// {app_id:"18cac37a",app_key:"b6cd34e621dd6c06efde7e5a82419d68"}
 export const APP_ID = '82fcd8fd'
 export const APP_KEY = 'ffe68b916e42ac50cda93e9e6bcbbd6d'
 export const GALLERY_NAME = 'phunsukhwangdu'
